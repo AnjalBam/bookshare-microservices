986f04a97eecbf0d26da0da2b16a474a751ff908
Anjal Bam <anjalbam81@gmail.com>
2022-12-01 15:27:55 +0545

    [feat] fixe minor

--- auth0/core/settings.py
+++ auth0/core/settings.py
@@ -72,20 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
-}
-
-OAUTH2_PROVIDER = {
-    # this is the list of available scopes
-    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
-}
-
+REST_FRAMEWORK = {}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
