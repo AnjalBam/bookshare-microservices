a23f1cbf775c77ccd96947688f44b18e2835c8fc
Anjal Bam <anjalbam81@gmail.com>
2023-04-12 00:53:24 +0545

    feat(chore): util function to get standard broadcast message format

--- store/helpers/utils.py
+++ store/helpers/utils.py
@@ -3,3 +3,11 @@ import uuid
 def get_uid_str():
     uid = str(uuid.uuid4())
     return uid.replace('-', "")
+
+
+def get_broadcast_message(event_type=None, data=None):
+    """A utility function to get the broadcast message following the standard convention"""
+    return {
+        "event": event_type,
+        "data": data
+    }
